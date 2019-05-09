954a4f466e836071134602ba2afcd97ae1520937
abtorres <abtorres079@gmail.com>
2019-05-09 11:17:08 -0600

    done all header stuff

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,33 @@
 import React from 'react';
 import './Header.css';
+
+class HeaderTitle extends React.Component {
+    state = {
+        day: "",
+        month: "",
+    };
+
+    componentDidMount() {
+        this.getDate();
+    }
+    
+    getDate = () => {
+        let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Oct', 'Nov', 'Dec'];
+        let date = new Date();
+        let day = date.getDate();
+        let month = date.getMonth();
+        month = months[month];
+        this.setState({day});
+        this.setState({month});
+    }
+
+    render() {
+        const {day} = this.state;
+        const {month} = this.state;
+        return (
+            <h1>Lambda School @Lambdaschool {month} {day}</h1>
+        );
+    }
+}
+
+export default HeaderTitle;
