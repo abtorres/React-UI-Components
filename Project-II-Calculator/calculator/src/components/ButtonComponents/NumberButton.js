4b5837b65065170b8b7a9cd1220b5b287fd104c5
abtorres <abtorres079@gmail.com>
2019-05-16 21:58:49 -0600

    finished with mvp

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = props => {
+    this.props = props;
+    return (
+        <button className={props.buttonStyle}>{props.text}</button> 
+    );
+}
+
+export default NumberButton;
